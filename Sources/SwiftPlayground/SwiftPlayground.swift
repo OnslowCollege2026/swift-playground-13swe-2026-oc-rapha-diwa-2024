3050d40f919ee32a1c0d3e63297a3663180028b2
rapha <159368583+oc-rapha-diwa-2024@users.noreply.github.com>
2026-02-12 22:03:11 +0000

    finnnnnn

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -3,19 +3,21 @@
 
 @main
 struct SwiftPlayground {
+    /// Sort scores.
     static func main() {
         let scores = [45, 78, 89, 32, 50, 92, 67, 41, 99, 56]
 
-        // cube number.
+        // Cube number.
         let total = scores.map {return $0 + 5}
-        // ilter even.
+        // Filter over 50.
         .filter {return $0 >= 50 }
-        // add temall        
-
+        // Add them all.
         let totally = total.reduce(0) { return $0 + $1 }
-        
+
+        // Number of scores in the array.
         var numOfScores = total.count
 
+        // Print total average.
         print(totally/numOfScores) // Output: 15
     }
 }
